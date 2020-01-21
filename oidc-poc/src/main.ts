985644187fb576a2359ffade97a70f2ba308a524
Aaron Schweig <42006873+aaronschweig@users.noreply.github.com>
2020-01-21 16:37:25 +0100

    POC for basic Machine-to-Machine authentication
    Find Solution for:
    * scope formatting
    * static scope  handling
    * static clients with fixed secrets?

--- oidc-poc/src/main.ts
+++ oidc-poc/src/main.ts
@@ -3,29 +3,36 @@ import { AppModule } from './app.module';
 import { Provider } from 'oidc-provider';
 
 const provider = new Provider('https://auth.ams-pro.de', {
-  // jwks --> TODO,
-  clients: [
-    {
-      client_id: 'foo',
-      client_secret: 'bar',
-      redirect_uris: ['https://example.com'],
-      response_types: ['id_token'],
-      grant_types: ['implicit'],
-      token_endpoint_auth_method: 'client_secret_basic',
+    jwks: keystore.toJWKS(true),
+    formats: { AccessToken: 'jwt', ClientCredentials: 'jwt', customizers: {
+        jwt(ctx, token, parts) {
+            // Hier Formatting von single-scopes zu zusammengefassten scopes
+            // --> ams:betriebsmittel:read ams:betriebsmittel:write -> ams:betriebsmittel:{read,write}
+        }
+    } },
+    cookies: { keys: ['test'] },
+    // features: { ietfJWTAccessTokenProfile: { enabled: true } },
+    features: {
+        clientCredentials: { enabled: true },
+        introspection: { enabled: true },
+        userinfo: { enabled: true },
     },
-  ],
-  formats: {
-    AccessToken: 'jwt',
-  },
-  features: {
-    introspection: { enabled: true },
-    revocation: { enabled: true },
-    encryption: { enabled: true },
-  },
-  cookies: {
-    keys: ['super', 'woopy', 'secret'],
-  },
-  extraParams: ['prompt'],
+
+    scopes: 'ams:betriebsmittel:read ams:betriebsmittel:write'.split(' '),
+
+    clients: [
+        {
+            client_id: 'betriebsmittel_api',
+            client_secret: 'bar',
+            redirect_uris: ['http://localhost:3001/cb'],
+            response_types: [],
+            grant_types: ['client_credentials'],
+            token_endpoint_auth_method: "client_secret_basic",
+            introspection_endpoint_auth_method: 'client_secret_basic',
+            scope: 'ams:betriebsmittel:read ams:betriebsmittel:write'
+        }
+    ],
+    // dynamicScopes: [/ams:[a-z]{1,}:(read|write)/],
 });
 
 async function bootstrap() {
