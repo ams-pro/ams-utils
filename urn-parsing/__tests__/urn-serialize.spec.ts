e7bc06d0bcb14a35411e26eca2ae9ffe4d7808a7
Aaron Schweig <42006873+aaronschweig@users.noreply.github.com>
2020-01-21 17:32:48 +0100

    Add files via upload

---
+++ urn-parsing/__tests__/urn-serialize.spec.ts
@@ -0,0 +1,24 @@
+import { AMSRolesInput, serializeToUrn } from './urn-serialize';
+
+describe('Test urn-serializer functionality', () => {
+
+    test('should serialize a simple urn', () => {
+        const input: AMSRolesInput[] = [
+            {
+                module: 'betriebsmittel',
+                read: true
+            },
+            {
+                module: 'betriebsmittel',
+                write: true
+            },
+            {
+                module: 'gefahrstoffe',
+                read: true,
+                write: true
+            }
+        ]
+        expect(serializeToUrn(input)).toEqual('ams:betriebsmittel:{read,write} ams:gefahrstoffe:{read,write}')
+    })
+
+})
